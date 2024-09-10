00c58ee7b502dd1bce2450ae7a9b09332e0cb41c
hyperb0rean <jewellin02@gmail.com>
2024-09-10 18:09:44 +0300

    matcher

--- matcher/src/main.rs
+++ matcher/src/main.rs
@@ -1,3 +1,67 @@
+
+use std::env;
+
+#[derive(Debug)]
+enum State {
+    S1,
+    S2,
+    S3,
+    S4,
+    S5,
+    Err
+}
+
+impl State {
+
+    fn next(self, sym: char) -> State {
+        match self {
+            State::S1 => match sym {
+                'a' => State::S2,
+                'c' => State::S3,
+                _ => State::Err
+            }, 
+            State::S2 => match sym {
+                'b' => State::S4,
+                _ => State::Err
+            },
+            State::S3 => match sym {
+                'a' => State::S4,
+                'b' => State::S5,
+                _ => State::Err
+            },
+            State::S4 => match sym {
+                _ => State::Err
+            },
+            State::S5 => match sym {
+                'b' => State::S5,
+                _ => State::Err
+            },
+            State::Err => State::Err
+        }
+    }
+
+    fn resolved(self) -> bool {
+        match self {
+            State::S1 => true, 
+            State::S3 => true,
+            State::S4 => true,
+            State::S5 => true,
+            _ => false
+        }
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        panic!("Wrong number of args!");
+    }
+    let mut state = State::S1;
+    for sym in args[1].chars() { 
+        state = state.next(sym);
+    }
+    if state.resolved() {
+        println!("Bingo!");
+    } else {
+        println!("No!");
+    }
 }

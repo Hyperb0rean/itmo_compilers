a43a357125d949c1608e36538799cf2ed19e3f9e
hyperb0rean <jewellin02@gmail.com>
2024-09-18 20:29:23 +0300

    vrode rabotaet

--- grammar/src/main.rs
+++ grammar/src/main.rs
@@ -29,18 +29,18 @@ impl Grammar {
             table: HashMap::new(),
         };
 
-
         /*
-        S → ACAbb
-        A → bA'
-        D → CbBD | ε
-        B → ε | b
-        C → bbE | c
-        E→ aAA | bbbbb
+        S -> ACAbb
+        A -> bD
+        D -> CbBD | ε
+        B -> ε | b
+        C -> bbE | c
+        E -> aAA | bbbbb
         */
+
         grammar.rules.insert(
             Symbol::NonTerminal('S'),
-            vec![vec![Symbol::NonTerminal('A'), Symbol::NonTerminal('C'), Symbol::NonTerminal('A'), Symbol::NonTerminal('B'), Symbol::NonTerminal('B')]],
+            vec![vec![Symbol::NonTerminal('A'), Symbol::NonTerminal('C'), Symbol::NonTerminal('A'), Symbol::Terminal('b'), Symbol::Terminal('b')]],
         );
         grammar.rules.insert(
             Symbol::NonTerminal('A'),
@@ -77,8 +77,24 @@ impl Grammar {
         }
 
 
+        self.first.get_mut(&Symbol::NonTerminal('S')).unwrap().insert(Symbol::Terminal('b'));
+
         self.first.get_mut(&Symbol::NonTerminal('A')).unwrap().insert(Symbol::Terminal('b'));
-        self.first.get_mut(&Symbol::NonTerminal('A')).unwrap().insert(Symbol::Epsilon);
+        self.first.get_mut(&Symbol::NonTerminal('A')).unwrap().insert(Symbol::Terminal('c'));
+
+        self.first.get_mut(&Symbol::NonTerminal('D')).unwrap().insert(Symbol::Terminal('b'));
+        self.first.get_mut(&Symbol::NonTerminal('D')).unwrap().insert(Symbol::Terminal('c'));
+        self.first.get_mut(&Symbol::NonTerminal('D')).unwrap().insert(Symbol::Epsilon);
+
+        self.first.get_mut(&Symbol::NonTerminal('B')).unwrap().insert(Symbol::Terminal('b'));
+        self.first.get_mut(&Symbol::NonTerminal('B')).unwrap().insert(Symbol::Epsilon);
+
+        self.first.get_mut(&Symbol::NonTerminal('C')).unwrap().insert(Symbol::Terminal('b'));
+        self.first.get_mut(&Symbol::NonTerminal('C')).unwrap().insert(Symbol::Terminal('c'));
+
+        self.first.get_mut(&Symbol::NonTerminal('E')).unwrap().insert(Symbol::Terminal('a'));
+        self.first.get_mut(&Symbol::NonTerminal('E')).unwrap().insert(Symbol::Terminal('b'));
+    
 
   }
 
@@ -89,16 +105,73 @@ impl Grammar {
 
         self.follow.get_mut(&Symbol::NonTerminal('S')).unwrap().insert(Symbol::EndMarker);
 
+        self.follow.get_mut(&Symbol::NonTerminal('A')).unwrap().insert(Symbol::Terminal('b'));
+        self.follow.get_mut(&Symbol::NonTerminal('A')).unwrap().insert(Symbol::Terminal('c'));
+
+        self.follow.get_mut(&Symbol::NonTerminal('D')).unwrap().insert(Symbol::Terminal('b'));
+        self.follow.get_mut(&Symbol::NonTerminal('D')).unwrap().insert(Symbol::Terminal('c'));
+
+        self.follow.get_mut(&Symbol::NonTerminal('B')).unwrap().insert(Symbol::Terminal('b'));
+        self.follow.get_mut(&Symbol::NonTerminal('B')).unwrap().insert(Symbol::Terminal('c'));
+        self.follow.get_mut(&Symbol::NonTerminal('B')).unwrap().insert(Symbol::Epsilon);
+
+        self.follow.get_mut(&Symbol::NonTerminal('C')).unwrap().insert(Symbol::Terminal('b'));
+
+        self.follow.get_mut(&Symbol::NonTerminal('E')).unwrap().insert(Symbol::Terminal('b'));
     }
 
     fn create_parsing_table(&mut self) {
 
-        self.table.insert(
-            (Symbol::NonTerminal('S'), Symbol::Terminal('b')),
-            vec![Symbol::NonTerminal('A'), Symbol::NonTerminal('C'), Symbol::NonTerminal('A'), Symbol::NonTerminal('B'), Symbol::NonTerminal('B')],
-        );
+        for (non_terminal, productions) in &self.rules {
+            for production in productions {
+                let first_set = self.get_first_of_production(production);
 
+                for terminal in &first_set {
+                    if *terminal != Symbol::Epsilon {
+                        self.table.insert((non_terminal.clone(), terminal.clone()), production.clone());
+                    }
+                }
+
+                if first_set.contains(&Symbol::Epsilon) {
+                    if let Some(follow_set) = self.follow.get(non_terminal) {
+                        for terminal in follow_set {
+                            self.table.insert((non_terminal.clone(), terminal.clone()), production.clone());
+                        }
+                    }
+                }
+            }
+        }
     }
+
+    fn get_first_of_production(&self, production: &Vec<Symbol>) -> HashSet<Symbol> {
+        let mut result = HashSet::new();
+
+        for symbol in production {
+            match symbol {
+                Symbol::Terminal(_) => {
+                    result.insert(symbol.clone());
+                    break;
+                }
+                Symbol::NonTerminal(_) => {
+                    if let Some(first_set) = self.first.get(symbol) {
+                        result = &result | first_set;
+
+                        if !first_set.contains(&Symbol::Epsilon) {
+                            break;
+                        }
+                    }
+                }
+                Symbol::Epsilon => {
+                    result.insert(Symbol::Epsilon);
+                    break;
+                }
+                _ => {}
+            }
+        }
+
+        result
+    }
+
 
     fn parse(&self, input: &String) -> bool {
         let mut stack: Vec<Symbol> = vec![Symbol::EndMarker, Symbol::NonTerminal('S')]; 
@@ -108,6 +181,7 @@ impl Grammar {
         let mut idx = 0;
 
         while let Some(top) = stack.pop() {
+            println!("{:?} ", top);
             if top == Symbol::EndMarker && input_chars[idx] == '$' {
                 return true;
             }

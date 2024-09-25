25f60fa99aa58de7ea112bb886e46677a7e72dfd
hyperb0rean <jewellin02@gmail.com>
2024-09-25 17:36:22 +0300

    grammar

--- frust/src/ast.rs
+++ frust/src/ast.rs
@@ -1,3 +1,59 @@
+/*
+<program> ::= <statement_list>
+
+<statement_list> ::= <statement>*
+
+<statement> ::= <let_statement>
+              | <assign_statement>
+              | <if_expression>
+              | <while_expression>
+
+<let_statement> ::= <let_expression> ";"
+
+<assign_statement> ::=  <assign_expression> ";"
+
+<block> ::= "{" <statement_list> "}"
+
+<expression> ::= <literal>
+               | <identifier>
+               | <binary_expression>
+               | <unary_expression>
+               | <let_expression>
+               | <if_expression>
+               | <while_expression>
+               | <assign_expression>
+
+<binary_expression> ::= <expression> <binary_operator> <expression>
+<binary_operator> ::= "+" | "-" | "*" | "/" | "%" 
+                   | "&&" | "||" 
+                   | "==" | "!=" | "<" | ">" | "<=" | ">="
+
+<unary_expression> ::= <unary_operator> <expression>
+<unary_operator> ::= "!" | "-"
+
+<let_expression> ::= "let" <identifier> ":" <type> "=" <expression>
+
+<if_expression> ::= "if" <expression> <block> ("else" <block>)?
+
+<while_expression> ::= "while" <expression> <block>
+
+<assign_expression> ::= <identifier> "=" <expression>
+
+<literal> ::= <number> | <boolean>
+<number> ::= <digit>+
+<boolean> ::= "true" | "false"
+
+<identifier> ::= <letter> (<letter> | <digit>)*
+
+<type> ::= "num" | "bool"
+
+<digit> ::= [0-9]
+<letter> ::= [a-zA-Z_]
+
+ */
+
+
+
 #[derive(Debug, PartialEq, Clone)]
 pub enum Expr {
     Number(i32),
